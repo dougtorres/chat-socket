e6863b80cfefe11aa917ca92fd14d4c683c027cb
Douglas Torres <douglastorr@gmail.com>
2016-06-21 12:50:03 -0300

    #Logando Usuário

--- src/ifpb/edu/br/classes/Cliente.java
+++ src/ifpb/edu/br/classes/Cliente.java
@@ -1,5 +1,36 @@
 package ifpb.edu.br.classes;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Cliente {
+	
+	public static void main(String[] args){
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Digite seu nome: ");
+	    String op = keyboard.nextLine();
+		Socket s;
+		try {
+			
+			s = new Socket("localhost", 5503);
+			DataOutputStream out = new DataOutputStream(s.getOutputStream());
+			out.writeUTF(op);
+			s.close();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+
 
 }
